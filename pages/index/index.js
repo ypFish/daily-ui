8688d26b193427a37a088f9eb91a24c49636cf66
Peng.Yu <peng.yu@qyer.com>
2022-04-10 23:12:00 +0800

    feat: init

--- pages/index/index.js
+++ pages/index/index.js
@@ -2,7 +2,7 @@
  * @Description: 
  * @Author: yp
  * @Date: 2021-04-18 09:42:57
- * @LastEditTime: 2022-04-08 16:57:57
+ * @LastEditTime: 2022-04-10 23:00:17
  * @LastEditors: yp
  */
 // pages/set/set.js
@@ -22,39 +22,105 @@ Component({
    */
   data: {
 
-    //页面基础信息
-    baseInfo: {
-      //unionId
-      unionid: "",
-      //用户头像
-      nickName: "",
-      //用户昵称
-      avatarUrl: "",
-      //用户是否授权
-      userInfo: {
-        authorize: 0
+    //展开按钮
+    spread: false,
+    //全页弹窗展示
+    pageLoading: false,
+
+    //底部弹窗选择器
+    pickShow: false,
+    pickerValue: [2, 2],
+    pickerOptions: [
+      ['红色', '绿色', '黄色', '白色', '紫色', '黑色', '橙色'],
+      ['宝石', '珍珠', '翡翠', '玛瑙', '珊瑚', '钻石']
+    ],
+
+    listPickerShow: false,
+    listValue: 0,
+    //支持两种列表数据类型（数组列表和对象列表）
+    listOptions: [{
+        title: "巴黎",
+        desc: "法兰西共和国的首都，也是法国的政治和商业中心"
       },
-    },
-    //卡片配置列表
-    cardList: [],
-    //调整顺序卡片列表
-    cardListForSort: [],
-    //调整顺序全页配置开关
-    sortCoverPageVisible: false,
-    //聚焦状态
-    focusIdx: -1,
+      {
+        title: "华盛顿",
+        desc: "华盛顿哥伦比亚特区，美利坚合众国的首都"
+      },
+      {
+        title: "伦敦",
+        desc: "大不列颠及北爱尔兰联合王国首都，世界金融中心"
+      },
+      {
+        title: "伊斯坦布尔",
+        desc: "原名君士坦丁堡，是土耳其经济、文化、交通中心"
+      },
+      {
+        title: "巴西利亚",
+        desc: "巴西联邦共和国首都，是巴西第四大城市"
+      },
+      {
+        title: "东京",
+        desc: "日本首都，是面向东京湾的国际大都市"
+      },
+      {
+        title: "巴塞罗那",
+        desc: "位于伊比利亚半岛东北部，濒临地中海，是西班牙第二大城市"
+      },
+      {
+        title: "慕尼黑",
+        desc: "也称明兴，是德国巴伐利亚州的首府"
+      },
+      {
+        title: "悉尼",
+        desc: "位于澳大利亚的东南沿岸，是澳大利亚新南威尔士州的首府"
+      },
+      {
+        title: "布宜诺斯艾利斯",
+        desc: "阿根廷的首都和最大城市"
+      },
+      {
+        title: "莫斯科",
+        desc: "是俄罗斯联邦首都、莫斯科州首府"
+      }
+    ],
 
-    //位置列表
-    _positionList: [],
 
-    //基础信息同步完成时间戳（内容包含：用户选取的卡片[不含卡片数据]、用户信息、订阅信心、基础配置信息）
-    _baseInfoSyncTime: app.globalData.baseInfoSyncTime,
+    checkboxPickerShow: false,
+    chekboxValue: [1, 3, 4],
+    chekboxOptions: [
+      "美国·华盛顿",
+      "英国·伦敦",
+      "法国·巴黎",
+      "土耳其·伊斯坦布尔",
+      "巴西·巴西利亚",
+      "日本·东京",
+      "西班牙·巴塞罗那",
+      "韩国·汉城",
+      "澳大利亚·悉尼",
+      "阿根廷·布宜诺斯艾利斯",
+      "意大利·都灵",
+      "俄罗斯·莫斯科",
+    ],
 
-    //所有卡片数据同步完成完成
-    _cardInfoSyncTime: app.globalData.cardInfoSyncTime,
+    datePickerShow: false,
+    startTime: 1,
+    endTime: 1,
+    valueTime: -1,
 
-    //跳转顺序弹窗页
-    coverPageLoading: false
+    datePickerMDShow: false,
+    month: -1,
+    day: -1,
+
+    dialogShow: false,
+    dialogConfirm: true,
+
+    notifyVisible: false,
+    iconType: 0,
+    colorType: 0,
+    notifyTitle: "通知",
+
+    demo: -1
+
   },
 
   /**
@@ -62,9 +128,105 @@ Component({
    */
   methods: {
 
-    onLoad: function () {
+    onShow: function () {
 
+      const current = new Date();
+      const valueTime = +new Date();
+      const startTime = +new Date(current.getFullYear() - 1, current.getMonth(), current.getDate());
+      const endTime = +new Date(current.getFullYear() + 1, current.getMonth(), current.getDate());
+
+      const month = current.getMonth();
+      const day = current.getDate() - 1;
+
+      this.setData({
+        valueTime,
+        startTime,
+        endTime,
+        month,
+        day
+      })
     },
+
+    handleSpreadButtonTap() {
+      this.spread = !this.spread;
+      this.setData({
+        spread: this.spread
+      })
+    },
+
+    handleCoverPageLoading() {
+      this.setData({
+        pageLoading: true
+      }, setTimeout(() => {
+        this.setData({
+          pageLoading: false
+        })
+      }, 1200))
+    },
+
+    handlePickerConfirm(e) {
+      this.setData({
+        pickerValue: [e.detail.value[0], e.detail.value[1]]
+      })
+    },
+
+    handleListConfirm(e) {
+      this.setData({
+        listValue: e.detail.value
+      })
+    },
+
+    handleCheckboxConfirm(e) {
+      this.setData({
+        chekboxValue: e.detail.value
+      })
+    },
+
+    handleDatePickerConfirm(e) {
+      console.log(e.detail.value)
+      this.setData({
+        valueTime: e.detail.value
+      })
+    },
+
+    handleDatePickerMDConfirm(e) {
+      console.log(e.detail.value)
+      this.setData({
+        month: e.detail.value[0],
+        day: e.detail.value[1]
+      })
+    },
+
+    handleDialogShow() {
+      console.log('弹窗展示')
+      this.setData({
+        dialogShow: true
+      })
+    },
+
+    handleDialogConfirm(e) {
+      this.setData({
+        dialogConfirm: e.detail.value
+      })
+    },
+
+    handleNotify() {
+      this.setData({
+        notifyTitle: "操作成功！",
+        iconType: 0,
+        colorType: 0,
+        notifyVisible: true
+      })
+    },
+
+    handleDemoShow(e) {
+      const demo = e.currentTarget.dataset.demo ?? -1;
+      console.log('demo', demo)
+      this.setData({
+        demo: parseInt(demo),
+        spread: false
+      })
+    }
 
 
   }
